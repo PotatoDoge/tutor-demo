0100f839eef072dbc9599e302c2d66a445dab854
bprado <bastian.prado.v@gmail.com>
2023-11-12 12:48:34 -0800

    Improve error handling

--- src/main/java/com/bfp/tutordemo/response/GlobalExceptionHandler.java
+++ src/main/java/com/bfp/tutordemo/response/GlobalExceptionHandler.java
@@ -1,7 +1,39 @@
 package com.bfp.tutordemo.response;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.time.LocalTime.now;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public HttpResponse handleValidationException(MethodArgumentNotValidException ex) {
+
+        BindingResult bindingResult = ex.getBindingResult();
+        Map<String, String> validationErrors = new HashMap<>();
+
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return HttpResponse.builder()
+                .timestamp(now().toString())
+                .statusCode(400)
+                .status(HttpStatus.BAD_REQUEST)
+                .message("Validation failed")
+                .data(validationErrors)
+                .build();
+    }
+
 }
