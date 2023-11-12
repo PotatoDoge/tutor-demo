fd94cf334cd354051072913713f5bb46832307e4
bprado <bastian.prado.v@gmail.com>
2023-11-12 10:52:16 -0800

    Initial commit

---
+++ src/main/java/com/bfp/tutordemo/TutorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bfp.tutordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bfp/tutordemo/TutorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfp.tutordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
