dd1e7ce2f2af7eecd8fce8db5b4ee727e66da8aa
bprado <bastian.prado.v@gmail.com>
2023-11-13 19:03:46 -0800

    Add BaseEntityRepository interface

---
+++ src/main/java/com/bfp/tutordemo/repository/BaseEntityRepository.java
@@ -0,0 +1,9 @@
+package com.bfp.tutordemo.repository;
+
+
+import com.bfp.tutordemo.entity.BaseEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BaseEntityRepository <T extends BaseEntity,D> extends JpaRepository<T,Long> {
+
+}
