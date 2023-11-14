dd82ce760f3800cb70493f310e99872e9ad34344
bprado <bastian.prado.v@gmail.com>
2023-11-13 22:21:32 -0800

    Add BaseEntityDTO class to StudentDTO

--- src/main/java/com/bfp/tutordemo/entity/dto/StudentDTO.java
+++ src/main/java/com/bfp/tutordemo/entity/dto/StudentDTO.java
@@ -1,24 +1,8 @@
 package com.bfp.tutordemo.entity.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class StudentDTO {
+public class StudentDTO extends BaseEntityDTO{
 
-    @NotBlank(message = "First name should not be blank")
-    private String firstName;
-
-    @NotBlank(message = "Last name should not be blank")
-    private String lastName;
-
-    @NotNull
-    @Email(message = "Please provide a valid email address")
-    private String email;
 }
