67a5bbe2e762859cf99f16b303c6f66374136e6c
bprado <bastian.prado.v@gmail.com>
2023-11-13 18:49:44 -0800

    Add BaseEntityService interface

---
+++ src/main/java/com/bfp/tutordemo/service/BaseEntityService.java
@@ -0,0 +1,15 @@
+package com.bfp.tutordemo.service;
+
+import com.bfp.tutordemo.entity.BaseEntity;
+
+public interface BaseEntityService <T extends BaseEntity, D> {
+
+    T save(D entity);
+
+    T findById(Long id);
+
+    Iterable<T> findAll();
+
+    void delete(Long id);
+
+}
