f103e4af48d3f34fd42355e0d43cc274f08e3e88
bprado <bastian.prado.v@gmail.com>
2023-11-13 18:21:45 -0800

    Implement correctly basic CRUD service calls in controller

--- src/main/java/com/bfp/tutordemo/controller/StudentController.java
+++ src/main/java/com/bfp/tutordemo/controller/StudentController.java
@@ -2,36 +2,78 @@ package com.bfp.tutordemo.controller;
 
 import com.bfp.tutordemo.entity.dto.StudentDTO;
 import com.bfp.tutordemo.response.HttpResponse;
+import com.bfp.tutordemo.service.impl.StudentServiceImpl;
 import jakarta.validation.*;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import static java.time.LocalTime.now;
+import static java.util.Map.of;
 
 @RestController()
 @RequestMapping("students")
 @AllArgsConstructor
 public class StudentController {
 
+    private final StudentServiceImpl studentService;
+
     @GetMapping
-    public String getStudents(){
-        return "Students";
+    public HttpResponse getStudents(){
+        return HttpResponse
+                .builder()
+                .timestamp(now().toString())
+                .data(of("students",studentService.getStudents()))
+                .message("Students retrieved!")
+                .status(HttpStatus.OK)
+                .statusCode(HttpStatus.OK.value())
+                .build();
     }
     @GetMapping("{id}")
-    public String getStudentById(@PathVariable("id") Long id){
-        return "Students by id";
+    public HttpResponse getStudentById(@PathVariable("id") Long id){
+        return HttpResponse
+                .builder()
+                .timestamp(now().toString())
+                .data(of("student",studentService.getStudent(id)))
+                .message("Student retrieved!")
+                .status(HttpStatus.OK)
+                .statusCode(HttpStatus.OK.value())
+                .build();
     }
 
     @PostMapping
     public HttpResponse createStudent(@Valid @RequestBody StudentDTO student){
-        return HttpResponse.builder().message(student.toString()).build();
+        return HttpResponse
+                .builder()
+                .timestamp(now().toString())
+                .data(of("student",studentService.saveStudent()))
+                .message("Student created!")
+                .status(HttpStatus.CREATED)
+                .statusCode(HttpStatus.CREATED.value())
+                .build();
     }
 
     @PutMapping("{id}")
     public HttpResponse updateStudent(@PathVariable("id")Long id, @Valid @RequestBody StudentDTO student){
-        return HttpResponse.builder().message(student.toString()).build();
+        return HttpResponse
+                .builder()
+                .timestamp(now().toString())
+                .data(of("student",studentService.updateStudent(id)))
+                .message("Student updated!")
+                .status(HttpStatus.OK)
+                .statusCode(HttpStatus.OK.value())
+                .build();
     }
 
     @DeleteMapping("{id}")
-    public String deleteStudentById(@PathVariable("id")Long id){
-        return "delete student";
+    public HttpResponse deleteStudentById(@PathVariable("id")Long id){
+        studentService.deleteStudent(id);
+        return HttpResponse
+                .builder()
+                .timestamp(now().toString())
+                .message("Student deleted!")
+                .status(HttpStatus.NO_CONTENT)
+                .statusCode(HttpStatus.NO_CONTENT.value())
+                .build();
     }
 }
