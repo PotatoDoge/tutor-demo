5bb7951a4066eb0efe9d368cc9daf1f433fc5f65
bprado <bastian.prado.v@gmail.com>
2023-11-13 21:01:33 -0800

    Add cross origin port listener

--- src/main/java/com/bfp/tutordemo/controller/StudentController.java
+++ src/main/java/com/bfp/tutordemo/controller/StudentController.java
@@ -14,6 +14,7 @@ import static java.util.Map.of;
 @RestController()
 @RequestMapping("students")
 @AllArgsConstructor
+@CrossOrigin("http://localhost:3000")
 public class StudentController {
 
     private final StudentService studentService;
