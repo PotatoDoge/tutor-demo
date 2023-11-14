590a40a34c348040a7e70502c3b260692cf68168
bprado <bastian.prado.v@gmail.com>
2023-11-13 19:11:56 -0800

    Connect service to repository

--- src/main/java/com/bfp/tutordemo/service/impl/StudentService.java
+++ src/main/java/com/bfp/tutordemo/service/impl/StudentService.java
@@ -2,39 +2,49 @@ package com.bfp.tutordemo.service.impl;
 
 import com.bfp.tutordemo.entity.Student;
 import com.bfp.tutordemo.entity.dto.StudentDTO;
+import com.bfp.tutordemo.repository.impl.StudentRepository;
 import com.bfp.tutordemo.service.BaseEntityService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class StudentService implements BaseEntityService<Student,StudentDTO> {
+
+    private final StudentRepository studentRepository;
 
     private final Student s = new Student(1L,"bastian","prado","mail@mail.com");
 
     @Override
     public Student save(StudentDTO studentDTO) {
-        return s;
+        Student student = new Student(null,studentDTO.getFirstName(), studentDTO.getLastName(), studentDTO.getEmail());
+        return studentRepository.save(student);
     }
 
     @Override
-    public Student findById(Long id) {
-        return s;
+    public Optional<Student> findById(Long id) {
+        return studentRepository.findById(id);
     }
 
     @Override
     public List<Student> findAll() {
-        return Collections.singletonList(s);
+        return studentRepository.findAll();
     }
 
     @Override
     public void delete(Long id) {
+        studentRepository.deleteById(id);
         System.out.println("deleted");
     }
 
     @Override
     public Student update(Long id, StudentDTO studentDTO) {
-        return s;
+        Student student = new Student(id,studentDTO.getFirstName(), studentDTO.getLastName(), studentDTO.getEmail());
+        studentRepository.save(student);
+        return student;
     }
 }
