e88fad9aca71b785e72841b246d237b4f12198f2
bprado <bastian.prado.v@gmail.com>
2023-11-13 22:31:17 -0800

    Add TutorRepository class

---
+++ src/main/java/com/bfp/tutordemo/repository/impl/TutorRepository.java
@@ -0,0 +1,8 @@
+package com.bfp.tutordemo.repository.impl;
+
+import com.bfp.tutordemo.entity.Tutor;
+import com.bfp.tutordemo.entity.dto.TutorDTO;
+import com.bfp.tutordemo.repository.BaseEntityRepository;
+
+public interface TutorRepository extends BaseEntityRepository<Tutor, TutorDTO> {
+}
