63efcda62505d02d269fbb6ae69e8ad47b1eeb4c
bprado <bastian.prado.v@gmail.com>
2023-11-13 19:04:03 -0800

    Add StudentRepository implementation

---
+++ src/main/java/com/bfp/tutordemo/repository/impl/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.bfp.tutordemo.repository.impl;
+
+import com.bfp.tutordemo.entity.Student;
+import com.bfp.tutordemo.entity.dto.StudentDTO;
+import com.bfp.tutordemo.repository.BaseEntityRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends BaseEntityRepository<Student, StudentDTO> {
+}
