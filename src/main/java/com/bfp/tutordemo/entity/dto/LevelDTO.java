eabea5a171f3abd4fb36c1f4b80d98be8afed629
bprado <bastian.prado.v@gmail.com>
2023-11-15 22:48:42 -0800

    Add validation to LevelDTO

--- src/main/java/com/bfp/tutordemo/entity/dto/LevelDTO.java
+++ src/main/java/com/bfp/tutordemo/entity/dto/LevelDTO.java
@@ -1,10 +1,12 @@
 package com.bfp.tutordemo.entity.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LevelDTO {
 
+    @NotBlank(message = "The name of the level should not be blank")
     private String name;
 
 }
