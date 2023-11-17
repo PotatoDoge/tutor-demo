efb3cb99234626095aa10ff2cf6b7945feae2302
bprado <bastian.prado.v@gmail.com>
2023-11-16 21:01:18 -0800

    Add validation to AppointmentDTO

--- src/main/java/com/bfp/tutordemo/entity/dto/AppointmentDTO.java
+++ src/main/java/com/bfp/tutordemo/entity/dto/AppointmentDTO.java
@@ -1,5 +1,7 @@
 package com.bfp.tutordemo.entity.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -7,7 +9,9 @@ import java.time.LocalDateTime;
 @Data
 public class AppointmentDTO {
 
+    @NotBlank(message = "Description should not be blank")
     private String description;
+
+    @NotNull(message = "Appointment's date and time should not be null nor blank")
     private LocalDateTime appointmentDateTime; // this will always be saved in UTC
-    private String timezone;
 }
