55977dbf81159b6dc555cc62afee387a071807e1
bprado <bastian.prado.v@gmail.com>
2023-11-18 22:38:48 -0800

    Add new repository method

--- src/main/java/com/bfp/tutordemo/repository/impl/SubjectLevelRepository.java
+++ src/main/java/com/bfp/tutordemo/repository/impl/SubjectLevelRepository.java
@@ -1,9 +1,14 @@
 package com.bfp.tutordemo.repository.impl;
 
+import com.bfp.tutordemo.entity.Level;
+import com.bfp.tutordemo.entity.Subject;
 import com.bfp.tutordemo.entity.linkingTables.SubjectLevel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SubjectLevelRepository extends JpaRepository<SubjectLevel,Long> {
+
+    public boolean existsBySubjectAndLevel(Subject subject, Level level);
+
 }
