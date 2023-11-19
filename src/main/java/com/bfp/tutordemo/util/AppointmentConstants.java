1767a763687ec43271d7a147939b7ac8bd4782dc
bprado <bastian.prado.v@gmail.com>
2023-11-18 21:50:21 -0800

    Add appointment constants

---
+++ src/main/java/com/bfp/tutordemo/util/AppointmentConstants.java
@@ -0,0 +1,8 @@
+package com.bfp.tutordemo.util;
+
+public class AppointmentConstants {
+
+    public final static String FREE_APPOINTMENT = "FREE";
+    public final static String TAKEN_APPOINTMENT = "TAKEN";
+
+}
