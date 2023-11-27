e048473fdcbbecc49386a4059ff92b9806b944d4
bprado <bastian.prado.v@gmail.com>
2023-11-27 09:42:24 -0800

    Add validation to not accept appointments that are already taken

--- src/main/java/com/bfp/tutordemo/service/impl/TutorAppointmentService.java
+++ src/main/java/com/bfp/tutordemo/service/impl/TutorAppointmentService.java
@@ -4,8 +4,12 @@ import com.bfp.tutordemo.entity.Appointment;
 import com.bfp.tutordemo.entity.Tutor;
 import com.bfp.tutordemo.entity.linkingTables.TutorAppointment;
 import com.bfp.tutordemo.repository.impl.TutorAppointmentRepository;
+import com.bfp.tutordemo.response.exception.ValueExistsInDatabase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.bfp.tutordemo.util.AppointmentConstants.FREE_APPOINTMENT;
+import static com.bfp.tutordemo.util.AppointmentConstants.TAKEN_APPOINTMENT;
 
 @Service
 @RequiredArgsConstructor
@@ -17,12 +21,17 @@ public class TutorAppointmentService {
 
     public TutorAppointment acceptAppointment(Long appointmentId, Long tutorId){
         Appointment appointment = appointmentService.findById(appointmentId).orElse(null);
+        if (appointment != null && !appointment.getStatus().equals(FREE_APPOINTMENT)) {
+            throw new ValueExistsInDatabase("The appointment is already taken!");
+        }
         Tutor tutor = tutorService.findById(tutorId);
         TutorAppointment tutorAppointment = new TutorAppointment();
         tutorAppointment.setTutor(tutor);
         tutorAppointment.setAppointment(appointment);
         tutorAppointment.setStatus("ACCEPTED");
-        return tutorAppointmentRepository.save(tutorAppointment);
+        tutorAppointment = tutorAppointmentRepository.save(tutorAppointment);
+        appointmentService.changeStatus(appointmentId, TAKEN_APPOINTMENT);
+        return tutorAppointment;
     }
 
 }
