0a983bc2e89299ce57356aba25574767cf0d619f
bprado <bastian.prado.v@gmail.com>
2023-12-30 12:21:13 -0800

    Change AppointmentController responses to ResponseEntity

--- src/main/java/com/bfp/tutordemo/controller/AppointmentController.java
+++ src/main/java/com/bfp/tutordemo/controller/AppointmentController.java
@@ -7,6 +7,7 @@ import com.bfp.tutordemo.service.impl.TutorAppointmentService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static java.time.LocalTime.now;
@@ -15,81 +16,69 @@ import static java.util.Map.of;
 @RestController
 @RequestMapping("appointments")
 @AllArgsConstructor
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("http://localhost:4200")
 public class AppointmentController {
 
     private final AppointmentService appointmentService;
     private final TutorAppointmentService tutorAppointmentService;
 
     @GetMapping
-    public HttpResponse getAppointments(@RequestParam(name = "subject", required = false)String subject, @RequestParam(name = "level", required = false) String level){
-        return HttpResponse
+    public ResponseEntity<HttpResponse> getAppointments(@RequestParam(name = "subject", required = false)String subject, @RequestParam(name = "level", required = false) String level){
+        return ResponseEntity.ok(HttpResponse
                 .builder()
                 .timestamp(now().toString())
                 .data(of("appointments",appointmentService.findAll(subject,level)))
                 .message("Appointments retrieved!")
-                .status(HttpStatus.OK)
-                .statusCode(HttpStatus.OK.value())
-                .build();
+                .build());
     }
     @GetMapping("{id}")
-    public HttpResponse getAppointmentById(@PathVariable("id") Long id){
-        return HttpResponse
+    public ResponseEntity<HttpResponse> getAppointmentById(@PathVariable("id") Long id){
+        return ResponseEntity.ok(HttpResponse
                 .builder()
                 .timestamp(now().toString())
                 .data(of("appointment",appointmentService.findById(id)))
                 .message("Appointment retrieved!")
-                .status(HttpStatus.OK)
-                .statusCode(HttpStatus.OK.value())
-                .build();
+                .build());
     }
 
     @PostMapping("student/{studentId}/subjectLevel/{subjectLevelId}")
-    public HttpResponse createAppointment(@PathVariable("studentId") Long studentId, @PathVariable("subjectLevelId") Long subjectLevelId, @RequestBody @Valid AppointmentDTO appointmentDTO){
-        return HttpResponse
+    public ResponseEntity<HttpResponse> createAppointment(@PathVariable("studentId") Long studentId, @PathVariable("subjectLevelId") Long subjectLevelId, @RequestBody @Valid AppointmentDTO appointmentDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(HttpResponse
                 .builder()
                 .timestamp(now().toString())
                 .data(of("appointment",appointmentService.save(studentId,subjectLevelId, appointmentDTO)))
                 .message("Appointment created!")
-                .status(HttpStatus.CREATED)
-                .statusCode(HttpStatus.CREATED.value())
-                .build();
+                .build());
     }
 
     @PutMapping("{id}")
-    public HttpResponse updateAppointment(@PathVariable("id")Long id, @Valid @RequestBody AppointmentDTO appointment){
-        return HttpResponse
+    public ResponseEntity<HttpResponse> updateAppointment(@PathVariable("id")Long id, @Valid @RequestBody AppointmentDTO appointment){
+        return ResponseEntity.ok(HttpResponse
                 .builder()
                 .timestamp(now().toString())
                 .data(of("appointment",appointmentService.update(id,appointment)))
                 .message("Appointment updated!")
-                .status(HttpStatus.OK)
-                .statusCode(HttpStatus.OK.value())
-                .build();
+                .build());
     }
 
     @DeleteMapping("{id}")
-    public HttpResponse deleteSubjectById(@PathVariable("id")Long id){
+    public ResponseEntity<HttpResponse> deleteSubjectById(@PathVariable("id")Long id){
         appointmentService.delete(id);
-        return HttpResponse
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(HttpResponse
                 .builder()
                 .timestamp(now().toString())
                 .message("Appointment deleted!")
-                .status(HttpStatus.NO_CONTENT)
-                .statusCode(HttpStatus.NO_CONTENT.value())
-                .build();
+                .build());
     }
 
     @PostMapping("{appointmentId}/accept/tutor/{tutorId}")
-    public HttpResponse acceptAppointment(@PathVariable("appointmentId") Long appointmentId, @PathVariable("tutorId")Long tutorId){
-        return HttpResponse
+    public ResponseEntity<HttpResponse> acceptAppointment(@PathVariable("appointmentId") Long appointmentId, @PathVariable("tutorId")Long tutorId){
+        return ResponseEntity.status(HttpStatus.CREATED).body(HttpResponse
                 .builder()
                 .timestamp(now().toString())
                 .data(of("tutorAppointment",tutorAppointmentService.acceptAppointment(appointmentId,tutorId)))
                 .message("Appointment accepted by a tutor!")
-                .status(HttpStatus.CREATED)
-                .statusCode(HttpStatus.CREATED.value())
-                .build();
+                .build());
     }
 
 }
